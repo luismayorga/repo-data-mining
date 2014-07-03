3c3ed3d6e605f9c567dc31bd3169c71886cc2342
Luis Mayorga <luismayorgam@gmail.com>
2014-07-03 22:00:29 +0200

    checkout

--- src/nose/core.clj
+++ src/nose/core.clj
@@ -1,7 +1,12 @@
 (ns nose.core
   (:gen-class)
-  (:require [clojure.java.shell :as shell])
-  (:import (org.tmatesoft.svn.core.wc2 SvnOperationFactory)))
+  (:require [clojure.java.shell :as shell]
+            [clojure.string])
+  (:import (java.lang System)
+           (java.io File)
+           (org.tmatesoft.svn.core.wc2 SvnOperationFactory 
+                                       SvnTarget)
+           (org.tmatesoft.svn.core.wc SVNRevision)))
 
 (def lang "java")
 
@@ -18,6 +23,7 @@
 
 
 (defn run-infusion
+  "paths given to inFusionC must be absolute"
   [infusion-path repo-copy-path xml-output]
   (shell/sh infusion-path "-lang" lang "-path" repo-copy-path "-hudsonreport" xml-output))
 
@@ -25,8 +31,32 @@
 (defn -main
   [infusion-path repo-path xml-output-dir]
   (let [op-factory (new SvnOperationFactory)
-        checkout (.createCheckout op-factory)]
+        module-name    (apply 
+                         #(nth % (dec (count %)))
+                         (vector (clojure.string/split repo-path #"/"))) 
 
-    ;(run-infusion infusion-path repo-path (compose-xml-path xml-output-dir (name-xml "results" "1")))
-    ))
+        checkout (.createCheckout op-factory)
+        revision (SVNRevision/create 0)
+        source (SvnTarget/fromFile
+                 (new File repo-path) revision)
+        target (SvnTarget/fromFile 
+                 (doto 
+                   (File/createTempFile "nose" (System/currentTimeMillis))
+                   .mkdir))]
+    (do
+      (.setSource checkout source)
+      (.setSingleTarget target) 
+      (.run checkout)
+      (run-infusion infusion-path repo-path (compose-xml-path
+                                              xml-output-dir
+                                              (name-xml 
+                                                module-name
+                                                "0")))
+      (loop [i 1]
+        ;TODO
+        ;change revision
+        ;update
+        ;run infusion
+        )
+      (.dispose op-factory))))
 
